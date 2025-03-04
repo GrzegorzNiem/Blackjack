0856dfa6c285e784bfdff448cf5fc68fbb1c654b
GrzegorzNiem <grzegorz.niemczyk2001@gmail.com>
2025-03-04 11:39:37 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,105 @@
+############### Blackjack Project #####################
+
+#Difficulty Normal 😎: Use all Hints below to complete the project.
+#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
+#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
+#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
+
+############### Our Blackjack House Rules #####################
+
+## The deck is unlimited in size.
+## There are no jokers.
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+## The computer is the dealer.
+
+import random
+import art
+import os
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+
+def draw():
+    drawing = random.choice(cards)
+    return drawing
+def ace(aced_score, hand):
+    for i in range(len(hand)):
+        if hand[i] == 11 and aced_score > 21:
+            aced_score -= 10
+            hand[i] = 1
+    print(hand)
+    return aced_score,hand
+def pc_decision(pc_score,pc_cards):
+    while pc_score < 17:
+        new_card_pc = draw()
+        pc_cards.append(new_card_pc)
+        pc_score += new_card_pc
+        pc_score, pc_cards = ace(pc_score,pc_cards)
+        return pc_score, pc_cards
+def game():
+    human_cards = []
+    pc_cards = []
+    begin = input("Do you wanna play Blackjack game: 'y' or 'n' ")
+    if begin == 'y':
+        first_card_pc = draw()
+        pc_cards.append(first_card_pc)
+        second_card_pc = draw()
+        pc_cards.append(second_card_pc)
+        pc_score=sum(pc_cards)
+
+
+        first_card = draw()
+        human_cards.append(first_card)
+        second_card = draw()
+        human_cards.append(second_card)
+        score = sum(human_cards)
+        os.system('cls')
+        print(art.logo)
+        print(f"{human_cards}, current score: {score} ")
+        print(f"Computer's first card: {first_card_pc}")
+        over_21 = False
+        while not over_21:
+            another_card = input("Type 'y' to get another card, type 'n' to pass: ")
+            if another_card == 'y':
+                new_card = draw()
+                print(new_card)
+                human_cards.append(new_card)
+                score+=new_card
+                score,human_cards = ace(score,human_cards)
+
+                print(f"You drew {new_card}")
+                print(f"{human_cards}, current score: {score} ")
+                print(f"Computer's first card: {first_card_pc}")
+                if score > 21:
+                    print("Bust! \nYou lost!")
+                    over_21 = True
+                    game()
+            if another_card == 'n':
+                if not pc_score > 17:
+                    pc_score, pc_cards = pc_decision(pc_score,pc_cards)
+                print(f"{human_cards}, current score: {score} ")
+                print(f"Computer's final hand: {pc_cards}, {pc_score}")
+                if pc_score <= 21:
+                    if score == pc_score:
+                        print("draw")
+                    elif score > pc_score:
+                        print("You win!")
+                    elif score < pc_score:
+                        print("You lost!")
+                    game()
+                elif pc_score > 21:
+                    print("You win!")
+                    game()
+
+
+
+
+game()
+
+
+
